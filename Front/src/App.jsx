24a4f6853397d6153b150ef0ee4b889e545e7085
Gerardo Leiva Diaz <a01198479@tec.mx>
2025-03-10 12:36:33 -0600

    API de prueba 1

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Router } from 'react-router-dom'
 import './css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLogin, setIsLogin] = useState(() => {
+    return localStorage.getItem('isLogin') === 'true' ? useState(true) : useState(false);
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" // Ruta default
+            element={isLogin ? <Navigate to='/home' /> : <Navigate to='/login'/>} // Protección de rutas
+          />
+          <Route path="/login" // Ruta login
+            element={isLogin ? <Navigate to='/home' /> : <Login /* Agregar funcion para login fetch */ />}
+          />
+          <Route path="/home" // Ruta home
+            element={isLogin ? <Home /> : <Navigate to='/login' />} // Protección de rutas
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
