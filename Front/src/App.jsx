ce56f8b78f40d2c6e60e222a092eb55c3f289fa9
Gerardo Leiva Diaz <a01198479@tec.mx>
2025-03-10 12:59:17 -0600

    login random

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,29 +1,44 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Router } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
 import './css/App.css'
 
 function App() {
   const [isLogin, setIsLogin] = useState(() => {
-    return localStorage.getItem('isLogin') === 'true' ? useState(true) : useState(false);
+    // return localStorage.getItem('token') ? true : false;
+    return false;
   });
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" // Ruta default
-            element={isLogin ? <Navigate to='/home' /> : <Navigate to='/login'/>} // Protección de rutas
+          <Route
+            path="/" // Ruta default
+            element={
+              isLogin ? <Navigate to="/home" /> : <Navigate to="/login" />
+            } // Protección de rutas
           />
-          <Route path="/login" // Ruta login
-            element={isLogin ? <Navigate to='/home' /> : <Login /* Agregar funcion para login fetch */ />}
+          <Route
+            path="/login" // Ruta login
+            element={
+              isLogin ? <Navigate to="/home" /> : <Login /* Agregar funcion para login fetch, la que está es de prueba */ setIsLogin={setIsLogin} /> // Pasamos setIsLogin como prop
+            }
           />
-          <Route path="/home" // Ruta home
-            element={isLogin ? <Home /> : <Navigate to='/login' />} // Protección de rutas
+          <Route 
+            path="/home" // Ruta home
+            element={isLogin ? <Home /> : <Navigate to="/login" />} // Protección de rutas
+          /> 
+          <Route
+            path="*" // Ruta no encontrada
+            element={<Navigate to="/" />}
           />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
--- Front/src/pages/Login.jsx
+++ Front/src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export default function Login() {
+const Login = ({setIsLogin}) => { // Funcion anónima para recibir la función *setIsLogin* desde App.jsx
   return (
     <div>
-      
+        <h1>Login</h1>
+        <button onClick={() => setIsLogin(true)}>Iniciar Sesión</button> 
     </div>
   )
 }
 
+export default Login
