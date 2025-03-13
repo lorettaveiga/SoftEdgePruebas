f1e9313db3b523e5773a42d04b64e2e2ae526f28
Gerardo Leiva Diaz <a01198479@tec.mx>
2025-03-12 19:24:49 -0600

    gané la pelea contra el API de login

--- Back/index.js
+++ Back/index.js
@@ -1,18 +1,20 @@
-
 import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 import express from "express";
 import indexRoutes from "./routes/index.routes.js";
+import loginRoutes from "./routes/login.routes.js";
 
 const app = express();
-
-
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use("/", indexRoutes);
+app.use(indexRoutes);
+app.use(loginRoutes);
 
-app.listen(process.env.BACK_PORT, console.log(`Server is running on port ${process.env.BACK_PORT}`));
+app.listen(
+  process.env.BACK_PORT,
+  console.log(`Server is running on port ${process.env.BACK_PORT}`)
+);
