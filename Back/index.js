1f96e4917dddd3eca7e442e6cce93ca021a874f5
lorettaveiga <a00835854@tec.mx>
2025-05-23 00:29:54 -0600

    bb

--- Back/index.js
+++ Back/index.js
@@ -34,6 +34,11 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   })
 );
 app.use(morgan("dev"));
